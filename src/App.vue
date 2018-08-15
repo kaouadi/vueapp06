ebba9df9829b2bbffe5ba92c5283c4656287469d
Karim Aouadi <kdolphinus@gmail.com>
2018-08-15 15:15:40 +0200

    Updated

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Global/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Global from '@/components/Global.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Global
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Global.vue
@@ -0,0 +1,23 @@
+<template>
+
+<div class="container">
+    <h4 class="hide">Hide me on all screen size</h4>
+    <h4 class="hide-on-small-only">Hide me on small screen size</h4>
+    <h4 class="hide-on-med-only">Hide me on medium screen size</h4>
+    <h4 class="hide-on-large-only">Hide me on large screen size</h4>
+    <h4 class="hide-on-med-and-down">Hide me on medium and small screen size</h4>
+    <h4 class="hide-on-med-and-up">Hide me on medium and large screen size</h4>
+</div>
+    
+</template>
+
+<script>
+export default {
+    name: 'Global'
+
+}
+</script>
+
+<style>
+
+</style>
